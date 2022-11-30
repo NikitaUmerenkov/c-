ebde063259e1cbe3ad4488ae3d274674c393a0da
Nikita Umerenkov <nekit2000basket@mail.ru>
2022-11-30 21:26:31 +0800

    решение 4 задач в дз

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿/* //Задача 2
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < number2)
+{
+    Console.Write(number2);
+}
+else
+{
+    Console.Write(number1);
+}
+*/
+
+
+/*//Задача 4
+Console.WriteLine("Введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 3");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+if (number1 < number2)
+{
+    max = number2;
+}
+else
+{
+    max = number1;
+}
+
+if (max < number3)
+{
+    max = number3;
+}
+else
+{
+
+}
+
+Console.Write(max);
+*/
+
+
+
+/*//Задача 6
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("число является четным");
+}
+else
+{
+    Console.WriteLine("число является нечетным");
+}
+*/
+
+
+
+/*
+//Задача 8
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 1;
+
+while (num1 <= num)
+{
+    if (num1 % 2 == 0)
+    {
+        Console.Write(num1);
+        num1 = num1 +1;
+    }
+    else
+    {
+        num1 = num1 +1;
+    }
+}
+*/
